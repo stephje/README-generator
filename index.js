02d326522a74266971b0efb493600b690d619688
stephje <s.jenkins3018@gmail.com>
2021-06-25 14:24:14 +0800

    Updated to include editor and basic input options

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 const generateMarkdown = require('./utils/generateMarkdown.js');
 
+//List of common licenses
 const licenses = [
     'MIT',
     'GNU GPLv3',
@@ -12,88 +13,119 @@ const licenses = [
     'Apache License 2.0',
     'Boost Software License 1.0',
     'The Unlicense',
-  ];
+];
 
 const questions = [
-  {
-    type: 'input',
-    name: 'title',
-    message: 'Please enter the title of your project.',
-  },
-  {
-    type: 'input',
-    name: 'description',
-    message: 'Please enter a description for your project.',
-  },
-  {
-    type: 'input',
-    name: 'username',
-    message: 'Please enter your GitHub username',
-  },
-  {
-    type: 'input',
-    name: 'email',
-    message: 'Please enter your email address',
-  },
-  {
-    type: 'list',
-    loop: false,
-    name: 'license',
-    message: 'Please select a license from the list',
-    choices: licenses,
-  },
-  {
-    type: 'editor',
-    name: 'installation',
-    message:
-      'Please enter any installation instructions required to use your project',
-  },
-  {
-    type: 'editor',
-    name: 'contribution',
-    message: 'Please enter instructions on how to contribute to your project',
-  },
-  {
-    type: 'editor',
-    name: 'tests',
-    message:
-      'Please enter instructions on how to run the tests for your project',
-  },
-  {
-    type: 'editor',
-    name: 'usage',
-    message: 'Please enter any usage instructions for your project',
-  },
+    {
+        type: 'list',
+        choices: ['Editor', 'Basic Inputs'],
+        message:
+            'Would you like to use your default editor, or stick to basic command line inputs?',
+        name: 'editor',
+    },
+    {
+        type: 'input',
+        name: 'title',
+        message: 'Please enter the title of your project.',
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Please enter a description for your project.',
+    },
+    {
+        type: 'input',
+        name: 'username',
+        message: 'Please enter your GitHub username',
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Please enter your email address',
+    },
+    {
+        type: 'list',
+        loop: false,
+        name: 'license',
+        message: 'Please select a license from the list',
+        choices: licenses,
+    },
+    {
+        type: 'editor',
+        name: 'installation',
+        message:
+            'Please enter any installation instructions required to use your project',
+        when: answers => answers.editor === 'Editor',
+    },
+    {
+        type: 'editor',
+        name: 'contribution',
+        message:
+            'Please enter instructions on how to contribute to your project',
+        when: answers => answers.editor === 'Editor',
+    },
+    {
+        type: 'editor',
+        name: 'tests',
+        message:
+            'Please enter instructions on how to run the tests for your project',
+        when: answers => answers.editor === 'Editor',
+    },
+    {
+        type: 'editor',
+        name: 'usage',
+        message: 'Please enter any usage instructions for your project',
+        when: answers => answers.editor === 'Editor',
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message:
+            'Please enter any installation instructions required to use your project',
+        when: answers => answers.editor === 'Basic Inputs',
+    },
+    {
+        type: 'input',
+        name: 'contribution',
+        message:
+            'Please enter instructions on how to contribute to your project',
+        when: answers => answers.editor === 'Basic Inputs',
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message:
+            'Please enter instructions on how to run the tests for your project',
+        when: answers => answers.editor === 'Basic Inputs',
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Please enter any usage instructions for your project',
+        when: answers => answers.editor === 'Basic Inputs',
+    },
 ];
 
 // Write README file
 function writeToFile(fileName, data) {
-  //A conditional using a ternary operator. Condition is 'if there is an error', if truthy the error is logged to the console, if falsy then "Success!" is logged to the console
-  fs.writeFile(fileName, data, (err) =>
-    err
-      ? console.error(err)
-      : console.log('README.md file generated successfully')
-  );
+    //A conditional using a ternary operator. Condition is 'if there is an error', if truthy the error is logged to the console, if falsy then "Success!" is logged to the console
+    fs.writeFile(fileName, data, err =>
+        err
+            ? console.error(err)
+            : console.log('README.md file generated successfully')
+    );
 }
 
 // TODO: Create a function to initialize app
-function init() {
-  inquirer
-    .prompt(questions)
-    .then((answers) => {
-      let markdown = generateMarkdown(answers, licenses);
-      writeToFile('README_gen.md', markdown)
-    })
-    .catch((error) => {
-      if (error.isTtyError) {
-        //Error message for when there is no tty. This is included in the inquirer documentation as an explicit error case
-        console.log('Prompt could not be rendered');
-      } else {
-        //Any other error
-        console.log('An unknown error occurred:', error);
-      }
-    });
+async function main() {
+    //prompt user for answers to questions and store answers as variable
+    let answers = await inquirer.prompt(questions);
+
+    //Generate the markdown for the README file
+    let markdown = generateMarkdown(answers, licenses);
+
+    //Write the generated markdown to a file
+    writeToFile('Generated_README.md', markdown);
 }
 
-// Function call to initialize app
-init();
+main();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,22 +1,22 @@
 //from https://gist.github.com/lukas-h/2a5d00690736b4c3a7ba
-const badges = [
-  '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)',
-  '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)',
-  '[![License: AGPL v3](https://img.shields.io/badge/License-AGPL%20v3-blue.svg)](https://www.gnu.org/licenses/agpl-3.0)',
-  '[![License: LGPL v3](https://img.shields.io/badge/License-LGPL%20v3-blue.svg)](https://www.gnu.org/licenses/lgpl-3.0)',
-  '[![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)',
-  '[![License: Apache 2.0](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)',
-  '[![License: Boost Software License 1.0](https://img.shields.io/badge/License-Boost%201.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)',
-  '[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)',
-];
+
+const badges = {
+  'MIT': '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)',
+  'GNU GPLv3': '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)',
+  'GNU AGPLv3': '[![License: AGPL v3](https://img.shields.io/badge/License-AGPL%20v3-blue.svg)](https://www.gnu.org/licenses/agpl-3.0)',
+  'GNU LGPLv3': '[![License: LGPL v3](https://img.shields.io/badge/License-LGPL%20v3-blue.svg)](https://www.gnu.org/licenses/lgpl-3.0)',
+  'Mozilla Public License 2.0': '[![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)',
+  'Apache License 2.0': '[![License: Apache 2.0](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)',
+  'Boost Software License 1.0': '[![License: Boost Software License 1.0](https://img.shields.io/badge/License-Boost%201.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)',
+  'The Unlicense': '[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)',
+}
 
 // Generate the markdown for the README
 function generateMarkdown(answers, licenses) {
   console.log(answers);
 
   let selectedLicense = answers.license;
-  let licenseIndex = licenses.indexOf(selectedLicense);
-  let badge = badges[licenseIndex];
+  let badge = badges.selectedLicense;
   let github = `[GitHub](https://github.com/${answers.username})`;
   let email = `[Email](mailto:${answers.email})`;
 
